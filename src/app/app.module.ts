fe9384b90fc23eeeae41d5a594c0d06243fff0b2
Alexis Wanja <alexis.wanja@student.moringaschool.com>
2022-02-07 10:25:03 +0300

    generate pipe time count and add if else function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HighlightVotesDirective } from './highlight-votes.directive';
+import { TimeCountPipe } from './time-count.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HighlightVotesDirective } from './highlight-votes.directive';
     QuoteComponent,
     QuoteDetailsComponent,
     NavBarComponent,
-    HighlightVotesDirective
+    HighlightVotesDirective,
+    TimeCountPipe
   ],
   imports: [
     BrowserModule,
