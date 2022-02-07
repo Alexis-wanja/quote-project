98f41137665a6275c496198ea7e86e7e5c1dee5c
Alexis Wanja <alexis.wanja@student.moringaschool.com>
2022-02-07 10:21:58 +0300

    generate highlight vote directive and add constructor element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HighlightVotesDirective } from './highlight-votes.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    NavBarComponent
+    NavBarComponent,
+    HighlightVotesDirective
   ],
   imports: [
     BrowserModule,
